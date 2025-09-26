632fb23870aaa1563030f1b34a6d5d1d3cf64395
Jorge Gonzalez <alex.developer.18x@gmail.com>
2025-09-26 17:30:07 -0600

    Integrated react router

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -6,27 +6,6 @@ interface Props {
 
 const ExpenseList = ({ expenses }: Props) => {
   return (
-    // <div>
-    //   <table border={1}>
-    //     <thead>
-    //       <tr>
-    //         <th>Title</th>
-    //         <th>Amount</th>
-    //         <th>Date</th>
-    //       </tr>
-    //     </thead>
-    //     <tbody>
-    //       {expenses.map((expense) => (
-    //         <tr key={expense.expenseId}>
-    //           <td>{expense.name}</td>
-    //           <td>{expense.amount}</td>
-    //           <td>{expense.date}</td>
-    //         </tr>
-    //       ))}
-    //     </tbody>
-    //   </table>
-    // </div>
-
     <div className="card">
       <h5 className="card-header">
         Expense
@@ -34,15 +13,17 @@ const ExpenseList = ({ expenses }: Props) => {
       </h5>
       <div className="card-body">
         {expenses.map((expense) => (
-          <div className="d-flex justify-content-between border-bottom-1 p-3 text-dark">
-            <div className="card-title m-0">
-              <h5>{expense.name}</h5>
-              <span className="fst-italic">{expense.date.toString()}</span>
-            </div>
-            <div className="card-subtitle">
-              <span className="badge rounded-pill app-primary-bg-color">
-                {expense.amount}
-              </span>
+          <div key={expense.expenseId}>
+            <div className="d-flex justify-content-between border-bottom-1 p-3 text-dark">
+              <div className="card-title m-0">
+                <h5>{expense.name}</h5>
+                <span className="fst-italic">{expense.date.toString()}</span>
+              </div>
+              <div className="card-subtitle">
+                <span className="badge rounded-pill app-primary-bg-color">
+                  {expense.amount}
+                </span>
+              </div>
             </div>
           </div>
         ))}
